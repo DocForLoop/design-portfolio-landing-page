7895be14c93f648036f3cf521163d6b08f7afe12
Csaba Molnar <docforloop@proton.me>
2025-05-15 23:37:34 +0200

    Refactor TS

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,21 @@ import './scss/style.scss';
 import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
 import Splide from '@splidejs/splide';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const splide = new Splide('.splide', {
-    type: 'loop',
-    perPage: 1,
-    gap: '2rem',
-    start: 2,
-    focus: 'center',
-    arrows: false,
-    speed: 1200,
-    
-    breakpoints: {
-      767: {
-        perPage: 1,
-        gap: '1rem',
-        start: 2,
-      },
+const splide = new Splide('.splide', {
+  type: 'loop',
+  perPage: 1,
+  gap: '2rem',
+  start: 2,
+  focus: 'center',
+  arrows: false,
+  speed: 1200,
+  breakpoints: {
+    767: {
+      perPage: 1,
+      gap: '1rem',
+      start: 2,
     },
+  },
 });
 
 splide.mount();
@@ -26,11 +24,8 @@ splide.mount();
 const btnPrev = document.querySelector('.splide__btn--prev');
 const btnNext = document.querySelector('.splide__btn--next');
 
-btnPrev!.addEventListener('click', () => {
-  splide.go('<');
-});
+const goToPrevSlide = () => splide.go('<');
+const goToNextSlide = () => splide.go('>');
 
-btnNext!.addEventListener('click', () => {
-  splide.go('>');
-})
-});
+btnPrev!.addEventListener('click', goToPrevSlide);
+btnNext!.addEventListener('click', goToNextSlide);
